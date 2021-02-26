ab2761d276cc4e04b95072d69ac4a471ca30f938
Dilana777 <dylana7792@hotmail.com>
2021-02-26 19:18:43 +0000

    subiendo archivos

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pinta = ["\u2666", "\u2665", "\u2660", "\u2663"];
+let numeros = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let picture = pinta[Math.floor(Math.random() * pinta.length)];
+  let number = numeros[Math.floor(Math.random() * numeros.length)];
+  document.querySelector("#numeros").innerHTML = number;
+  document.querySelector("#arribas").innerHTML = picture;
+  document.querySelector("#abajos").innerHTML = picture;
+  if (picture == "\u2666" || picture == "\u2665")
+    document.body.style.color = "red";
 };
